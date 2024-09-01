b4fc544679118f8bb795be1c6621f30b61e76f24
MohamedKadi <gamesmohamed14@gmail.com>
2024-09-01 16:55:06 +0100

    first time ajax

---
+++ script.js
@@ -0,0 +1,22 @@
+const button = document.getElementById('joke-btn');
+const theJoke = document.getElementById('joke');
+
+button.addEventListener('click', getDadJoke);
+
+
+function getDadJoke(){
+    
+    var myRequest = new XMLHttpRequest();
+    
+    myRequest.open('GET',"https://api.chucknorris.io/jokes/random", true);
+
+    myRequest.onreadystatechange = function() {
+        if(this.readyState === 4 && this.status === 200){
+            // Change the text content of the h1 element
+            var myObj = JSON.parse(this.response);
+            theJoke.textContent = myObj.value;
+        }
+    }
+    
+    myRequest.send();
+}
